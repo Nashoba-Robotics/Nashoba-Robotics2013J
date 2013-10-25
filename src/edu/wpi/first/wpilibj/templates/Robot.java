a7dfcc08be422b490414d7b754eab45f3ea5cf04
Colin <colinw@savix.net>
2013-10-25 13:14:56 -0400

    Added Garry's init code

---
+++ src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -0,0 +1,93 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.templates.commands.*;
+import edu.wpi.first.wpilibj.templates.subsystems.*;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+    Command autonomousCommand;
+    ArticulatingArm articulatingArm;
+    Drive drive;
+    FixedArm fixedArm;
+    PneumaticsCompressor pneumaticsCompressor;
+    Tipper tipper;
+    Shooter shooter;
+    Track track;
+    
+    double lastExecuteTime;
+    double runTime;
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+        // instantiate the command used for the autonomous period
+        autonomousCommand = new AutonomousCommand();
+        articulatingArm = new ArticulatingArm();
+        drive = new Drive();
+        fixedArm = new FixedArm();
+        pneumaticsCompressor = new PneumaticsCompressor();
+        tipper = new Tipper();
+        shooter = new Shooter();
+        track = new Track();
+        
+        // Initialize all subsystems
+        CommandBase.init();
+        SmartDashboard.putData(drive);
+    }
+
+    public void autonomousInit() {
+        if(autonomousCommand != null)
+            autonomousCommand.start();
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    public void teleopInit() {
+	// This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to 
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if(autonomousCommand != null)
+            autonomousCommand.cancel();
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+        //LiveWindow.run();
+    }
+}
